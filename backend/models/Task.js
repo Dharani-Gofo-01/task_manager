33a37a6c227071c172dfcb7497438d0b2a082e77
Dharani-Gofo-01 <dkgofo01@gmail.com>
2025-07-26 22:43:24 +0530

    Update Task.js

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -11,14 +11,3 @@ const taskSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('Task', taskSchema);
 
-
-
-// models/Task.js
-// const taskSchema = new mongoose.Schema({
-//   title: String,
-//   description: String,
-//   status: { type: String, enum: ['todo', 'in-progress', 'done'], default: 'todo' },
-//   assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-//   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-// });
-// module.exports = mongoose.model('Task', taskSchema);
