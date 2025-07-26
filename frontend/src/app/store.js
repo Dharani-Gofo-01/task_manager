f78b519be9a8569039dd104928d76af408baa6ca
Dharani-Gofo-01 <dkgofo01@gmail.com>
2025-07-26 23:51:42 +0530

    Update store.js

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import taskReducer from '../features/tasks/taskSlice';
 import commentReducer from '../features/comments/commentSlice';
-import usersReducer from "../features/users/usersSlice"; // ✅
+import usersReducer from "../features/users/usersSlice"; // 
 
 export const store = configureStore({
   reducer: {
