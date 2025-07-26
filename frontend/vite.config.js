a30c9e13db54a7c6f2448ecd114ec0d35a140618
Dharani-Gofo-01 <dkgofo01@gmail.com>
2025-07-26 23:09:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
+// import tailwindcss from 'tailwindcss';
 import path from 'path';
 
 // https://vitejs.dev/config/
