5b2bfd1c69c7d7db320643f3bc0e223c8cf1a9bc
Dharani-Gofo-01 <dkgofo01@gmail.com>
2025-07-26 22:42:10 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -34,30 +34,3 @@ module.exports = {
 };
 
 
-// authController.js
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
-// const User = require('../models/User');
-
-// exports.register = async (req, res) => {
-//   const { name, email, password, role } = req.body;
-//   const hashedPassword = await bcrypt.hash(password, 10);
-//   const user = new User({ name, email, password: hashedPassword, role });
-//   await user.save();
-//   res.status(201).json({ message: 'User registered successfully' });
-// };
-
-// exports.login = async (req, res) => {
-//   const { email, password } = req.body;
-//   const user = await User.findOne({ email });
-//   const isMatch = user && await bcrypt.compare(password, user.password);
-//   if (!isMatch) return res.status(401).json({ error: 'Invalid credentials' });
-
-//   const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET);
-//   res.json({ token });
-// };
-
-// exports.getMe = async (req, res) => {
-//   const user = await User.findById(req.user.id).select('-password');
-//   res.json(user);
-// };
