f6059a088274ded7e987167b7a4a6f3926538dda
Dharani-Gofo-01 <dkgofo01@gmail.com>
2025-07-26 22:42:45 +0530

    Update roleMiddleware.js

--- backend/middleware/roleMiddleware.js
+++ backend/middleware/roleMiddleware.js
@@ -1,13 +1,3 @@
-// module.exports = (role) => {
-//   return (req, res, next) => {
-//     if (req.user.role !== role)
-//       return res.status(403).json({ message: 'Access denied' });
-//     next();
-//   };
-// };
-
-
-
 // roleMiddleware.js
 const Task = require('../models/Task');
 
