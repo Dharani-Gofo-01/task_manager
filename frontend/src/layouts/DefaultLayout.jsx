682cd54a83a397224c83521119fdd915365dccf9
Dharani-Gofo-01 <dkgofo01@gmail.com>
2025-07-28 14:01:26 +0530

    Update DefaultLayout.jsx

--- frontend/src/layouts/DefaultLayout.jsx
+++ frontend/src/layouts/DefaultLayout.jsx
@@ -1,7 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Sidebar from '../components/Sidebar';
 
+
 export default function DefaultLayout() {
+    const { user } = useSelector((state) => state.auth);
+
+  //  If no user, redirect to login
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
   return (
     <div className="flex flex-col sm:flex-row min-h-screen bg-gray-100">
       <Sidebar />
