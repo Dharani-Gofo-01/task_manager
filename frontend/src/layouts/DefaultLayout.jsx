f99003955c3e934c240a40d0aab273a75cec85a8
Dharani-Gofo-01 <dkgofo01@gmail.com>
2025-07-28 14:07:10 +0530

    next

--- frontend/src/layouts/DefaultLayout.jsx
+++ frontend/src/layouts/DefaultLayout.jsx
@@ -4,21 +4,14 @@ import Sidebar from '../components/Sidebar';
 
 
 export default function DefaultLayout() {
-<<<<<<< HEAD
 
   const { user } = useSelector((state) => state.auth);
-=======
-    const { user } = useSelector((state) => state.auth);
->>>>>>> 682cd54a83a397224c83521119fdd915365dccf9
 
   //  If no user, redirect to login
   if (!user) {
     return <Navigate to="/login" />;
   }
-<<<<<<< HEAD
 
-=======
->>>>>>> 682cd54a83a397224c83521119fdd915365dccf9
   return (
     <div className="flex flex-col sm:flex-row min-h-screen bg-gray-100">
       <Sidebar />
